73108c9d1f850f0d47e5f4213b80112104fa9259
Maximalnii <zhukov.opt@yandex.ru>
2023-07-14 12:45:08 +0300

    create project

---
+++ SkyEng/SkyEng/SkyEng/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SkyEng
+//
+//  Created by Максим Жуков on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
