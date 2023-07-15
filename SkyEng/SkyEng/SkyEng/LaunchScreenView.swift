da454f6cf736291cc21e4ffc1b26be85050cde98
Maximalnii <zhukov.opt@yandex.ru>
2023-07-15 21:43:03 +0300

    secondLettleStep

---
+++ SkyEng/SkyEng/SkyEng/LaunchScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreenView.swift
+//  SkyEng
+//
+//  Created by Максим Жуков on 15.07.2023.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreenView()
+    }
+}
